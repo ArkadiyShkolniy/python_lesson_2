2b734cda449d65592ae675e500a99ee35de78fff
ARK <arkady.shkolniy@yandex.ru>
2020-02-01 17:07:41 +0300

    First Commit

---
+++ input_output.py
@@ -0,0 +1,19 @@
+# Однострочный крментарий
+'''
+многострочный коментарий
+'''
+
+
+print("Hello!!")
+print("Hello", "Student", 123)
+print("Hello", "Student", 123, sep = "xxx")
+print("Hello", "Student", 123, end = "oooooooo")
+print()
+
+# Ввод
+
+name = input("input your name")
+print(name, type(name))
+
+age = input("input your age")
+print(int(age), type(int(age)))
